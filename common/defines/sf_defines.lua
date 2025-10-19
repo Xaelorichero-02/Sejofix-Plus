7aef7a8ac773bfd4f608f25dd90091dbc30f4949
Xaelor <77115546+Xaelorichero-02@users.noreply.github.com>
2025-10-19 13:57:39 +0200

    WE LOVE SPEEEEED!
    not the drug ps

--- common/defines/sf_defines.lua
+++ common/defines/sf_defines.lua
@@ -1,3 +1,4 @@
 NDefines.NGame.END_DATE = "1953.1.1.1" --Cool yk --was:1949
+NDefines.NCountry.EVENT_PROCESS_OFFSET = 30 -- Extra performance duh --was:20
 NDefines.NAI.AI_UPDATE_ROLES_FREQUENCY_HOURS = 72 --Extra performance --was:48
 NDefines.NRaids.MAX_TARGETS_TO_UPDATE_PER_FRAME = 67 --Even more performance --was:100
