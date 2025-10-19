978b3b9ccd93284ca94bb2a9083f06a623e7f3a1
Xaelor <77115546+Xaelorichero-02@users.noreply.github.com>
2025-10-19 13:43:37 +0200

    CTD fix + log cleanup + more fixing

--- common/defines/sf_defines.lua
+++ common/defines/sf_defines.lua
@@ -1,3 +1,3 @@
-NDefines.NGame.END_DATE = "1953.1.1.1", --Cool yk --was:1949
-NDefines.NAI.AI_UPDATE_ROLES_FREQUENCY_HOURS = 72; --Extra performance --was:48
-NDefines.NRaids.MAX_TARGETS_TO_UPDATE_PER_FRAME = 67, --Even more performance --was:100
+NDefines.NGame.END_DATE = "1953.1.1.1" --Cool yk --was:1949
+NDefines.NAI.AI_UPDATE_ROLES_FREQUENCY_HOURS = 72 --Extra performance --was:48
+NDefines.NRaids.MAX_TARGETS_TO_UPDATE_PER_FRAME = 67 --Even more performance --was:100
